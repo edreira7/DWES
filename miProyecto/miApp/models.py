73eeb15eb756f30337116d87fa0665bf3b7cb5d0
Manuel <edreira20@gmail.com>
2025-01-19 18:11:33 +0100

    Creacion de la clase Evento

--- miProyecto/miApp/models.py
+++ miProyecto/miApp/models.py
@@ -12,4 +12,15 @@ bio = models.TextField(blank=True, null=True)
 def __str__(self):
     return self.username
 
+class Evento(models.Model):
+    titulo = models.CharField(max_length=200)
+    descroipcion = models.TextField()
+    date_time = models.DateTimeField()
+    capacidad = models.PositiveIntegerField()
+    image_url = models.URLField()
+    organizer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='eventos_organizados')
+
+
+def __str__(self):
+    return self.title
 
