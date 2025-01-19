e27ac3665d221036d1d69956ff6c0bdb5e2ad8b1
Manuel <edreira20@gmail.com>
2025-01-19 18:33:24 +0100

    Creacion de admin.register del CustomUser

--- miProyecto/miApp/admin.py
+++ miProyecto/miApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser, Evento, Reservas, Comentario
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'rol', 'bio')
+    list_filter = ('rol',)
+    search_fields = ('username','email')
+
